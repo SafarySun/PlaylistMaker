193486931d6fd29a057e4d533a3fcaca0ea71548
SafarySun <arakalandadze@gmail.com>
2023-11-03 09:41:40 +0600

    Second commit

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.practicum.playlistmaker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -8,13 +7,12 @@ import android.widget.ImageView
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings1)
+        setContentView(R.layout.activity_settings)
 
         val back = findViewById<ImageView>(R.id.backButton)
 
         back.setOnClickListener{
-            val backIntent = Intent(this,MainActivity::class.java)
-            startActivity(backIntent)
+          finish()
         }
     }
 }
