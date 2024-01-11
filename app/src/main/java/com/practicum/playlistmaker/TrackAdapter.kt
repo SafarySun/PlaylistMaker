08f2a04be8d4fa2c15250ffcd89d72014897cc61
SafarySun <arakalandadze@gmail.com>
2024-01-11 19:49:29 +0600

    Sprint_12-N1

--- app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -3,8 +3,9 @@ package com.practicum.playlistmaker
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TrackAdapter: RecyclerView.Adapter<TrackViewHolder>() {
+class TrackAdapter: RecyclerView.Adapter<TrackViewHolder>( ) {
      var tracks = ArrayList<Track>()
+     var clickListern:ClickListernForTrack? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
        return TrackViewHolder(parent)
     }
@@ -13,6 +14,15 @@ class TrackAdapter: RecyclerView.Adapter<TrackViewHolder>() {
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(tracks[position])
+        holder.itemView.setOnClickListener{
+            clickListern?.onTrackClickListern(tracks[position])
+           // clickListern?.onTrackClickListern(tracks[holder.adapterPosition])
+        }
+
 
     }
+}
+interface ClickListernForTrack{
+
+   fun onTrackClickListern(track:Track)
 }
